867c56b338ac7ad6328b55e7b4ce96f4a2ce72dd
AlexNguyen <nguyenhoainam121n@gmail.com>
2023-09-30 16:02:36 +0700

    Update: add ModalsProvider into index.js in src

--- f4education/frontend/src/index.js
+++ f4education/frontend/src/index.js
@@ -1,37 +1,62 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 
 // import MantineProvider from "@mantine/core";
 // import Notifications from "@mantine/notifications";
 // import "@mantine/notifications/styles.css";
 
-import "assets/plugins/nucleo/css/nucleo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/scss/argon-dashboard-react.scss";
-import "assets/css/costum-client-react.css";
+import 'assets/plugins/nucleo/css/nucleo.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'assets/scss/argon-dashboard-react.scss';
+import 'assets/css/costum-client-react.css';
 
-import AdminLayout from "layouts/Admin.js";
-import AuthLayout from "layouts/Auth.js";
-import ClientLayout from "layouts/Client.js";
-import { MantineProvider } from "@mantine/core";
+import AdminLayout from 'layouts/Admin.js';
+import AuthLayout from 'layouts/Auth.js';
+import ClientLayout from 'layouts/Client.js';
+import {MantineProvider} from '@mantine/core';
+import {ModalsProvider} from '@mantine/modals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <MantineProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin/*" element={<AdminLayout />} />
-        <Route path="/auth/*" element={<AuthLayout />} />
-        <Route path="/admin" element={<Navigate to="/admin/index" replace />} />
-        <Route path="/*" element={<ClientLayout />} />
-        <Route path="/admin/*/:courseName" element={<AdminLayout />} />
-        <Route
-          path="/admin/*/:courseName/:folderId"
-          element={<AdminLayout />}
-        />
-      </Routes>
-    </BrowserRouter>
-  </MantineProvider>
+	<BrowserRouter>
+		<MantineProvider>
+			<ModalsProvider>
+				<Routes>
+					<Route
+						path='/admin/*'
+						element={<AdminLayout />}
+					/>
+					<Route
+						path='/auth/*'
+						element={<AuthLayout />}
+					/>
+					<Route
+						path='/admin'
+						element={
+							<Navigate
+								to='/admin/index'
+								replace
+							/>
+						}
+					/>
+					<Route
+						path='/*'
+						element={<ClientLayout />}
+					/>
+					<Route
+						path='/admin/*/:courseName'
+						element={<AdminLayout />}
+					/>
+					<Route
+						path='/admin/*/:courseName/:folderId'
+						element={<AdminLayout />}
+					/>
+				</Routes>
+				,
+			</ModalsProvider>
+		</MantineProvider>
+		,
+	</BrowserRouter>,
 );
