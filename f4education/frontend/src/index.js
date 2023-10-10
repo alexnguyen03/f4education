c66e66a87788724d5954d0e9df74113cc6f9da10
2imLoi <khachhang1900571@gmail.com>
2023-10-10 14:42:42 +0700

    Add notifcation
    Hiển thị thông báo cho trường hợp load dữ liệu lên table lỗi
    Và trường hợp cập nhật dữ liệu không thành công và thành công

--- f4education/frontend/src/index.js
+++ f4education/frontend/src/index.js
@@ -1,63 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-// import MantineProvider from "@mantine/core";
+// import tineProvider from "@mantine/core";
 // import Notifications from "@mantine/notifications";
 // import "@mantine/notifications/styles.css";
 
-import 'assets/plugins/nucleo/css/nucleo.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'assets/scss/argon-dashboard-react.scss';
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-react.scss";
 
-import AdminLayout from 'layouts/Admin.js';
-import AuthLayout from 'layouts/Auth.js';
-import ClientLayout from 'layouts/Client.js';
-import {MantineProvider} from '@mantine/core';
-import {ModalsProvider} from '@mantine/modals';
+import AdminLayout from "layouts/Admin.js";
+import AuthLayout from "layouts/Auth.js";
+import ClientLayout from "layouts/Client.js";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<BrowserRouter>
-		<Routes>
-			<Route
-				path='/admin/*'
-				element={<AdminLayout />}
-			/>
-			<Route
-				path='/auth/*'
-				element={<AuthLayout />}
-			/>
-			<Route
-				path='/admin'
-				element={
-					<Navigate
-						to='/admin/index'
-						replace
-					/>
-				}
-			/>
-			<Route
-				path='/*'
-				element={<ClientLayout />}
-			/>
-			<Route
-				path='/course/*/:courseId'
-				element={<ClientLayout />}
-			/>
-			<Route
-				path='/*/course-register-detail/:courseId'
-				element={<ClientLayout />}
-			/>
-			<Route
-				path='/admin/*/:courseName'
-				element={<AdminLayout />}
-			/>
-			<Route
-				path='/admin/*/:courseName/:folderId'
-				element={<AdminLayout />}
-			/>
-		</Routes>
-	</BrowserRouter>,
+  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/*" element={<AdminLayout />} />
+        <Route path="/auth/*" element={<AuthLayout />} />
+        <Route path="/admin" element={<Navigate to="/admin/index" replace />} />
+        <Route path="/*" element={<ClientLayout />} />
+        <Route path="/course/*/:courseId" element={<ClientLayout />} />
+        <Route
+          path="/*/course-register-detail/:courseId"
+          element={<ClientLayout />}
+        />
+        <Route path="/admin/*/:courseName" element={<AdminLayout />} />
+        <Route
+          path="/admin/*/:courseName/:folderId"
+          element={<AdminLayout />}
+        />
+      </Routes>
+    </BrowserRouter>
+  
 );
